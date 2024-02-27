5279b7ae62ff28b5d2d592a1f235c7ed997fd198
mytirobo <imsumkr@gmail.com>
2024-02-27 15:00:03 +0530

    Learning Progress

---
+++ day-1-start.py
@@ -0,0 +1,12 @@
+#print(len(input("What is your name?")))
+name = "Sumit"
+print(name)
+
+name = "Salony"
+print(name)
+
+print(len(input("What is your name?")))
+
+name = input("What is your name?")
+length = len(name)
+print(length)
---
+++ day-2.py
@@ -0,0 +1,49 @@
+#Data Types
+# print("Hello"[4])
+# print("123"+"456")
+#Integer
+# print(123+456)
+# 123_456_789
+#Float
+3.14159
+#Boolean
+True
+False
+#Type Error, Type Checking and Type Conversion
+# num_char = len(input("What is your name?\n"))
+# print("Your name has " + num_char + " characters.")
+# print(type(num_char))
+# new_num_char = str(num_char)
+# print("Your name has " + new_num_char + " characters.")
+# name = "Sumit"
+# print(type(name))
+# a = float(123)
+# print(type(a))
+# print(70 + float("100.5"))
+# print(str(70) + str(100))
+
+# num = "32"
+# firstnum = num[0]
+# secondnum = num[1]
+# print(int(firstnum) + int(secondnum))
+
+# print(3 + 5)
+# 7 - 4
+# 3 * 2
+# print(type(6 / 3))
+# print(2 ** 3)
+
+# PEMDASLR
+# Parentheses ()
+# Exponents **
+# Multiplication *
+# Division /
+# Addition +
+# Subtraction -
+# Left to Right
+
+# print(3 * 3 + 3 / 3 - 3)
+# print(3 * (3 + 3) / 3 - 3)
+# print(type(4 / 2))
+# result = 4/2
+# print(result)

241359691887a3cf02fc6acd25622ab7f7c20607
mytirobo <imsumkr@gmail.com>
2024-02-29 15:09:29 +0530

    Learning Progress

--- day-4-Randomisation-PythonList.py
+++ day-4-Randomisation-PythonList.py
@@ -1,1 +1,30 @@
-import random
+# import random
+
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+
+# random_float = random.random() * 5
+# print(random_float)
+
+# states_of_india = ['Bihar', 'Karntaka', 'UP', 'J&k']
+# print(states_of_india[0])
+# print(states_of_india[-1])
+
+# states_of_india[1] = 'Karnataka'
+# print(states_of_india[1])
+
+# states_of_india.append('MP')
+# print(states_of_india)
+
+# states_of_india.extend(['Meghalaya', 'Sikkim'])
+# print(states_of_india)
+
+# print(len(states_of_india))
+# print(states_of_india[6])
+
+fruits = ['apple', 'banana']
+vegetable = ['cabbage', 'spinach']
+
+eatables = [fruits, vegetable]
+
+print(eatables)

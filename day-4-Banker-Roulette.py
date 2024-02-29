1e643c8f5c3775222b056e9be8d115658ea81543
mytirobo <imsumkr@gmail.com>
2024-02-29 15:09:06 +0530

    Learning Progress

---
+++ day-4-Banker-Roulette.py
@@ -0,0 +1,9 @@
+import random
+
+names = ['Salony', 'Sumit']
+num_items = len(names)
+
+random_choice = random.randint(0, num_items -1)
+person_who_will_pay = names[random_choice]
+
+print(person_who_will_pay + ' is going to buy the meal today!')

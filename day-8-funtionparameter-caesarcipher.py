33ef7e1e2d7d8e5759f534aa2f7f7335fc136df7
mytirobo <imsumkr@gmail.com>
2024-03-05 11:27:32 +0530

    Learning Progress

---
+++ day-8-funtionparameter-caesarcipher.py
@@ -0,0 +1,30 @@
+#Simple Function
+
+# def greet():
+#     print('Hello')
+#     print('How do you do?')
+#     print('Isn\'t the weather nice today?')
+
+# greet()
+
+#Function that allows for input
+
+# def greet_with_name(name):
+#     print(f'Hello {name}')
+#     print(f'How do you do {name}?')
+
+# greet_with_name('Salony')
+
+#name-parameter
+#Salony-argument
+
+#Function with more than 1 input
+
+def greet_with(name, location):
+    print(f'Hello {name}')
+    print(f'What is it like in {location}?')
+
+greet_with(location = 'Jamshedpur', name = 'Salony')
+
+#positional arugument, keyword argument
+
---
+++ day-8-paint-area-calculator.py
@@ -0,0 +1,13 @@
+import math
+
+def paint_calc(height, width, cover):
+  num_cans = (height * width) / cover
+  round_up_cans = math.ceil(num_cans)
+  print(f"You'll need {round_up_cans} cans of paint.")
+
+# Your code above this line 👆
+test_h = int(input('Enter the height: ')) # Height of wall (m)
+test_w = int(input('Enter the width: ')) # Width of wall (m)
+
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)

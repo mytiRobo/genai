304f1b2238ec018c49cbd4542dc3ab1e00404279
mytirobo <imsumkr@gmail.com>
2024-03-15 11:56:39 +0530

    Learning Progress

---
+++ day-18-tuples-importing_modules-turtle_graphics/turtle_graphics/main.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape('turtle')
+timmy.color('OliveDrab3')
+
+
+for _ in range(4):
+    timmy.forward(100)
+    timmy.left(90)
+
+screen = Screen()
+screen.exitonclick()

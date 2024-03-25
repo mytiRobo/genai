e82af4b33a81502f306c7a59946f014bc9262250
mytirobo <imsumkr@gmail.com>
2024-03-25 14:02:17 +0530

    learning progress

---
+++ hackerrank/arithmetic_operator.py
@@ -0,0 +1,17 @@
+
+a = int(input())
+b = int(input())
+
+def add(a, b):
+    return a+b
+    
+def diff(a, b):
+    return a-b
+    
+def prod(a, b):
+    return a*b
+
+print(add(a,b))
+print(diff(a,b))
+print(prod(a,b))
+

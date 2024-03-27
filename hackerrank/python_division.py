ba7c3316e5e271a7843ad7f950d4a70a28c8b119
mytirobo <imsumkr@gmail.com>
2024-03-27 08:05:38 +0530

    learning progress

---
+++ hackerrank/python_division.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+
+def int_division(a, b):
+    return a // b
+def float_division(a, b):
+    return a/b
+    
+print(int_division(a, b))
+print(float_division(a, b))
